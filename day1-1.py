15d6433e9d8c7c42ca8bf5f8140c3df1d0a7fc8b
Yong-Yu Huang <115194864+yongssh@users.noreply.github.com>
2023-12-01 21:23:06 -0600

    Create day1-1.py

---
+++ day1-1.py
@@ -0,0 +1,43 @@
+## replace path with actual file path
+file = open('path', 'r')
+input = file.readlines()
+
+## pt 1 ##
+
+filelength = len(input)
+numpair = [''] * filelength
+numpairindex = 0
+
+# array of strings without \n
+cleanedarray = [None] * filelength
+for i in range(filelength):
+    cleanedarray[i] = input[i].splitlines()
+
+for i in range(filelength):
+    string = str(cleanedarray[i])
+    strlen = len(string)
+    #print(string)
+    for j in range(strlen):
+        #print(string[j])
+        if string[j].isdigit():
+            numpair[numpairindex] += str(string[j])
+    numpairindex += 1
+    
+#print(numpair)
+
+finalnums = [0] * filelength
+index = 0
+for num in numpair: 
+    first = str(num[0])
+    last = str(num[-1]) 
+    finalnum = first + last   
+    finalnums[index] = int(finalnum)
+    index += 1
+#print(finalnums)    
+
+total = sum(finalnums)
+
+print(total)
+
+## pt 2 ##
+
